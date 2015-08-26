844fe0e75eb40edb06910f2c4c516e76824272e6
Vita V <dj.vita.09@gmail.com>
2015-08-25 21:53:13 -0500

    added boilerplate for swype like tinder

--- Adopt-A-Friend/Swype.m
+++ Adopt-A-Friend/Swype.m
@@ -7,6 +7,8 @@
 //
 
 #import "Swype.h"
+#import <MDCSwipeToChoose/MDCSwipeToChoose.h>
+//MIT licence, so we can ship in a commercial app!
 
 @interface Swype ()
 
@@ -14,9 +16,24 @@
 
 @implementation Swype
 
+#pragma mark - Creating and Customizing a MDCSwipeToChooseView
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    MDCSwipeToChooseViewOptions *options = [MDCSwipeToChooseViewOptions new];
+    options.likedText = @"Keep";
+    options.likedColor = [UIColor blueColor];
+    options.nopeText = @"Delete";
+    options.onPan = ^(MDCPanState *state){
+        if (state.thresholdRatio == 1.f && state.direction == MDCSwipeDirectionLeft) {
+            NSLog(@"Let go now to delete the photo!");
+        }
+    };
+    
+    MDCSwipeToChooseView *view = [[MDCSwipeToChooseView alloc] initWithFrame:self.view.bounds
+                                                                     options:options];
+    view.imageView.image = [UIImage imageNamed:@"photo"];
+    [self.view addSubview:view];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,15 +41,36 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - MDCSwipeToChooseDelegate Callbacks
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// This is called when a user didn't fully swipe left or right.
+- (void)viewDidCancelSwipe:(UIView *)view {
+    NSLog(@"Couldn't decide, huh?");
 }
-*/
+
+// Sent before a choice is made. Cancel the choice by returning `NO`. Otherwise return `YES`.
+- (BOOL)view:(UIView *)view shouldBeChosenWithDirection:(MDCSwipeDirection)direction {
+    if (direction == MDCSwipeDirectionLeft) {
+        return YES;
+    } else {
+        // Snap the view back and cancel the choice.
+        [UIView animateWithDuration:0.16 animations:^{
+            view.transform = CGAffineTransformIdentity;
+            view.center = [view superview].center;
+        }];
+        return NO;
+    }
+}
+
+// This is called then a user swipes the view fully left or right.
+- (void)view:(UIView *)view wasChosenWithDirection:(MDCSwipeDirection)direction {
+    if (direction == MDCSwipeDirectionLeft) {
+        NSLog(@"Photo deleted!");
+    } else {
+        NSLog(@"Photo saved!");
+    }
+}
+
 - (IBAction)btnBackPressed:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
